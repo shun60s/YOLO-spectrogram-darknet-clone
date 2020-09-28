b07b16495cbd4d9b6e21add1dbeeca8471873f0b
shun <36104188+shun60s@users.noreply.github.com>
2020-09-29 07:24:32 +0900

    revise

--- learn4-bat.sh
+++ learn4-bat.sh
@@ -9,6 +9,6 @@ wget https://pjreddie.com/media/files/darknet53.conv.74
 ./darknet detector train task1/voice.data task1/yolov3-obj.cfg darknet53.conv.74 -dont_show -map
 
 # reload
-./darknet detector train task1/voice.data task1/yolov3-obj.cfg task1/backup/yolov3-obj_last.weights -dont_show -map
+#./darknet detector train task1/voice.data task1/yolov3-obj.cfg task1/backup/yolov3-obj_last.weights -dont_show -map
 
 
